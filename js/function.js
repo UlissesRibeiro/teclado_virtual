7294db934e111b9a99cb67d427d752c25a5f8abe
ulisses gomes <ulissestark@gmail.com>
2022-08-15 07:21:03 -0300

    inicio do projeto

---
+++ js/function.js
@@ -0,0 +1,234 @@
+function digitar(event) {
+    var x = event.keyCode;
+  switch (x){
+        case 27:
+        document.getElementById("esc").style.backgroundColor = "green";
+        break;
+        case 112:
+        document.getElementById("f1").style.backgroundColor = "green";
+        break;
+        case 113:
+        document.getElementById("f2").style.backgroundColor = "green";
+        break;
+        case 114:
+        document.getElementById("f3").style.backgroundColor = "green";
+        break;
+        case 115:
+        document.getElementById("f4").style.backgroundColor = "green";
+        break;
+        case 116:
+        document.getElementById("f5").style.backgroundColor = "green";
+        break;
+        case 117:
+        document.getElementById("f6").style.backgroundColor = "green";
+        break;
+        case 118:
+        document.getElementById("f7").style.backgroundColor = "green";
+        break;
+        case 119:
+        document.getElementById("f8").style.backgroundColor = "green";
+        break;
+        case 120:
+        document.getElementById("f9").style.backgroundColor = "green";
+        break;
+        case 121:
+        document.getElementById("f10").style.backgroundColor = "green";
+        break;
+        case 122:
+        document.getElementById("f11").style.backgroundColor = "green";
+        break;
+        case 123:
+        document.getElementById("f12").style.backgroundColor = "green";
+        break;
+
+        case 192:
+        document.getElementById("aspas").style.backgroundColor = "green";
+        break;
+        case 48:
+        document.getElementById("num0").style.backgroundColor = "green";
+        break;
+        case 49:
+        document.getElementById("num1").style.backgroundColor = "green";
+        break;
+        case 50:
+        document.getElementById("num2").style.backgroundColor = "green";
+        break;
+        case 51:
+        document.getElementById("num3").style.backgroundColor = "green";
+        break;
+        case 52:
+        document.getElementById("num4").style.backgroundColor = "green";
+        break;
+        case 53:
+        document.getElementById("num5").style.backgroundColor = "green";
+        break;
+        case 54:
+        document.getElementById("num6").style.backgroundColor = "green";
+        break;
+        case 55:
+        document.getElementById("num7").style.backgroundColor = "green";
+        break;
+        case 56:
+        document.getElementById("num8").style.backgroundColor = "green";
+        break;
+        case 57:
+        document.getElementById("num9").style.backgroundColor = "green";
+        break;
+        case 189:
+        document.getElementById("hifen").style.backgroundColor = "green";
+        break;
+        case 187:
+        document.getElementById("igual").style.backgroundColor = "green";
+        break;
+        case 8:
+        document.getElementById("backspace").style.backgroundColor = "green";
+        break;
+
+        case 9:
+        document.getElementById("tab").style.backgroundColor = "green";
+        break;
+        case 81:
+        document.getElementById("q").style.backgroundColor = "green";
+        break;
+        case 87:
+        document.getElementById("w").style.backgroundColor = "green";
+        break;
+        case 69:
+        document.getElementById("e").style.backgroundColor = "green";
+        break;
+        case 82:
+        document.getElementById("r").style.backgroundColor = "green";
+        break;
+        case 84:
+        document.getElementById("t").style.backgroundColor = "green";
+        break;
+        case 89:
+        document.getElementById("y").style.backgroundColor = "green";
+        break;
+        case 85:
+        document.getElementById("u").style.backgroundColor = "green";
+        break;
+        case 73:
+        document.getElementById("i").style.backgroundColor = "green";
+        break;
+        case 79:
+        document.getElementById("o").style.backgroundColor = "green";
+        break;
+        case 80:
+        document.getElementById("p").style.backgroundColor = "green";
+        break;
+        case 219:
+        document.getElementById("agudo").style.backgroundColor = "green";
+        break;
+        case 221:
+        document.getElementById("colchete").style.backgroundColor = "green";
+        break;
+
+        
+        
+        case 16:
+        document.getElementById("shiftE").style.backgroundColor = "green";
+        break;
+        case 226:
+        document.getElementById("barra").style.backgroundColor = "green";
+        break;
+        case 90:
+        document.getElementById("z").style.backgroundColor = "green";
+        break;
+        case 88:
+        document.getElementById("x").style.backgroundColor = "green";
+        break;
+        case 67:
+        document.getElementById("c").style.backgroundColor = "green";
+        break;
+        case 86:
+        document.getElementById("v").style.backgroundColor = "green";
+        break;
+        case 66:
+        document.getElementById("b").style.backgroundColor = "green";
+        break;
+        case 78:
+        document.getElementById("n").style.backgroundColor = "green";
+        break;
+        case 77:
+        document.getElementById("m").style.backgroundColor = "green";
+        break;
+        case 188:
+        document.getElementById("virgula").style.backgroundColor = "green";
+        break;
+        case 190:
+        document.getElementById("ponto").style.backgroundColor = "green";
+        break;
+        case 191:
+        document.getElementById("doisPts").style.backgroundColor = "green";
+        break;
+        case 193:
+        document.getElementById("contraBarra").style.backgroundColor = "green";
+        break;
+
+        case 20:
+          document.getElementById("caps").style.backgroundColor = "green";
+          break;
+          case 65:
+          document.getElementById("a").style.backgroundColor = "green";
+          break;
+          case 83:
+          document.getElementById("s").style.backgroundColor = "green";
+          break;
+          case 68:
+          document.getElementById("d").style.backgroundColor = "green";
+          break;
+          case 70:
+          document.getElementById("f").style.backgroundColor = "green";
+          break;
+          case 71:
+          document.getElementById("g").style.backgroundColor = "green";
+          break;
+          case 72:
+          document.getElementById("h").style.backgroundColor = "green";
+          break;
+          case 74:
+          document.getElementById("j").style.backgroundColor = "green";
+          break;
+          case 75:
+          document.getElementById("k").style.backgroundColor = "green";
+          break;
+          case 76:
+          document.getElementById("l").style.backgroundColor = "green";
+          break;
+          case 186:
+          document.getElementById("cedilha").style.backgroundColor = "green";
+          break;
+          case 222:
+          document.getElementById("tio").style.backgroundColor = "green";
+          break;
+          case 13:
+          document.getElementById("enter").style.backgroundColor = "green";
+          break;
+
+        case 17:
+        document.getElementById("ctrlE").style.backgroundColor = "green";
+        break;
+        case 91:
+        document.getElementById("win").style.backgroundColor = "green";
+        break;
+        case 18:
+        document.getElementById("alt").style.backgroundColor = "green";
+        break;
+        case 32:
+        document.getElementById("space").style.backgroundColor = "green";
+        break;
+        /*tecla alt preparar um if case 18:
+        document.getElementById("altgr").style.backgroundColor = "yellow";
+        break;*/
+        case 86:
+        document.getElementById("v").style.backgroundColor = "green";
+        break;
+        case 93:
+        document.getElementById("menu").style.backgroundColor = "green";
+        break;
+         
+  
+
+  }
+}
